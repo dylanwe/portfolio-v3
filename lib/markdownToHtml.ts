80cc42d090d2ff871d5b87d185e22ba1aa4610f2
Dylan Weijgertze <dylanwprivate@gmail.com>
2023-04-07 13:16:03 +0200

    added prism highlighting

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,8 @@
 import { remark } from 'remark'
+import prism from 'remark-prism'
 import html from 'remark-html'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await remark().use(html, { sanitize: false }).use(prism).process(markdown)
   return result.toString()
 }
